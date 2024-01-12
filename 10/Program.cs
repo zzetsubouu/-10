d6c48268abd2230ec02814150685cbe6dc8bfdd1
zzetsubouu <154281553+zzetsubouu@users.noreply.github.com>
2024-01-13 01:07:37 +0300

    Add files via upload

---
+++ 10/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompPract10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Задача #1
+            //Console.Write("Введите номер дня недели: ");
+            //int num = int.Parse(Console.ReadLine());
+            //switch (num)
+            //{
+            //    case 1: Console.WriteLine("Понедельник");
+            //        break;
+            //    case 2: Console.WriteLine("Вторник");
+            //        break;
+            //    case 3: Console.WriteLine("Среда");
+            //        break;
+            //    case 4: Console.WriteLine("Четверг");
+            //        break;
+            //    case 5: Console.WriteLine("Пятница");
+            //        break;
+            //    case 6: Console.WriteLine("Суббота");
+            //        break;
+            //    case 7: Console.WriteLine("Воскресенье");
+            //        break;
+            //    default: Console.WriteLine("Введите номер от 1-7!");
+            //        break;
+            //}
+
+            //Задача #2 
+            //Console.Write("Введите номер месяца: ");
+            //int num = int.Parse(Console.ReadLine());
+            //switch (num)
+            //{
+            //    case 1:
+            //        Console.WriteLine("Январь");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("Февраль");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("Март");
+            //        break;
+            //    case 4:
+            //        Console.WriteLine("Апрель");
+            //        break;
+            //    case 5:
+            //        Console.WriteLine("Май");
+            //        break;
+            //    case 6:
+            //        Console.WriteLine("Июнь");
+            //        break;
+            //    case 7:
+            //        Console.WriteLine("Июль");
+            //        break;
+            //    case 8:
+            //        Console.WriteLine("Август");
+            //        break;
+            //    case 9:
+            //        Console.WriteLine("Сентябрь");
+            //        break;
+            //    case 10:
+            //        Console.WriteLine("Октябрь");
+            //        break;
+            //    case 11:
+            //        Console.WriteLine("Ноябрь");
+            //        break;
+            //    case 12:
+            //        Console.WriteLine("Декабрь");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Введен неправильный номер месяца. \nВыберите число в диапозоне от 1 до 12");
+            //        break;
+            //}
+
+            // Задача #3
+            //Console.Write("Введите число месяца: ");
+            //int num = int.Parse(Console.ReadLine());
+            //switch (num)
+            //{
+            //    case 1: case 2: case 12: Console.WriteLine("Это зима!");
+            //        break;
+            //    case 3: case 4: case 5: Console.WriteLine("Это весна!");
+            //        break;
+            //    case 6: case 7: case 8: Console.WriteLine("Это лето!");
+            //        break;
+            //    case 9: case 10: case 11: Console.WriteLine("Это весна!");
+            //        break;
+            //}
+
+            // Задача #4 ф – физика, м – математика, и – история, г – география, б – биология.
+            //Console.WriteLine("Введите первую букву предмета: ");
+            //string x = Console.ReadLine();
+            //switch (x)
+            //{
+            //    case "ф":Console.WriteLine("Физика");
+            //        break;
+            //    case "м": Console.WriteLine("Математика");
+            //        break;
+            //    case "и":Console.WriteLine("История");
+            //        break;
+            //    case "г": Console.WriteLine("География");
+            //        break;
+            //    case "б": Console.WriteLine("Биология");
+            //        break;
+            //}
+            Console.Read();
+        }
+    }
+}
